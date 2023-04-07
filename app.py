1357fea0eba47710e7cf98d29e176507b373386d
Ashish Khare <akstyles211@gmail.com>
2023-04-07 10:43:54 +0530

    Added gunicorn support

--- app.py
+++ app.py
@@ -1,19 +1,47 @@
 from flask import Flask, request
 import json
-from util.main import generate as g
+from pytube import YouTube as yt
+from time import time
+from waitress import serve
 
-app = Flask(__name__)
+def generate(id):
+    resp = yt('https://youtu.be/'+id)
+    info = resp.vid_info['videoDetails']
+    data = {}
+    data['created'] = round(time())
+    data['id'] = info['videoId']
+    data['title'] = info['title']
+    data['thumbnails'] = info['thumbnail']['thumbnails']
+    data['author'] = resp.author
+    info = resp.vid_info['streamingData']
+    data['streams'] = []
+    data['expiresInSeconds'] = info['expiresInSeconds']
+    data['expires'] = data['created'] + int(info['expiresInSeconds'])
+    streams = resp.streams.filter(mime_type='audio/webm').order_by(attribute_name='abr')
+    for i in streams:
+        info = {}
+        info['abrInKBPS'] = i.abr.replace('kbps','')
+        info['acodec'] = i.audio_codec
+        info['mime/type'] = i.mime_type
+        info['url'] = i.url
+        data['streams'].append(info)
+    return data
 
-@app.route("/id", methods=['POST'])
-def hello_world():
-    code = json.loads(request.data)['id']
-    return g(code)
 
-@app.route("/status")
-def getStatus():
-    return {
-        'status': 'working'
-    }
+def create_app():
+    app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    @app.route("/id", methods=['POST'])
+    def hello_world():
+        code = json.loads(request.data)['id']
+        return generate(code)
+
+    @app.route("/status")
+    def getStatus():
+        return {
+            'status': 'working'
+        }
+
+    return app
+
+serve(create_app)
